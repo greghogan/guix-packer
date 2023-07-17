2e244f709d74b820029d2922f5997ca51e7ea868
Greg Hogan <code@greghogan.com>
2023-07-17 16:57:46 +0000

    Register additional architectures with binfmt
    This patch refactors the binfmt registration to include PowerPC and
    32-bit ARM, and to register all architectures not equal to the current
    system.

--- scripts/finish.sh
+++ scripts/finish.sh
@@ -6,29 +6,45 @@ set -eufo pipefail
 
 ARCH=$(uname -m)
 
+function register_binfmt() {
+  CPU=$1
+  MAGIC=$2
+  MASK=$3
+
+  # registration permitted only after binaries are installed
+  echo ":qemu-${CPU}:M::${MAGIC}:${MASK}:/var/guix/profiles/per-user/offload/guix-profile/bin/qemu-${CPU}:F" > /proc/sys/fs/binfmt_misc/register
+}
+
 if "$INSTALL_EMULATION_BINARIES" ; then
   # register emulation binaries using the command from the qemu-binfmt-conf script as called by register.sh
   #   https://github.com/qemu/qemu/blob/master/scripts/qemu-binfmt-conf.sh
   #   https://github.com/multiarch/qemu-user-static/blob/master/containers/latest/register.sh
   # documented at https://docs.kernel.org/admin-guide/binfmt-misc.html
-  case "${ARCH}" in
-    aarch64)
-      CPU=x86_64
-      MAGIC='\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'
-      MASK='\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'
-      ;;
-    x86_64)
-      CPU=aarch64
-      MAGIC='\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00'
-      MASK='\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'
-      ;;
-    *)
-      echo "unsupported ARCH=${ARCH}"
-      exit 1
-  esac
+  if [ "${ARCH}" != "aarch64" ] ; then
+    CPU=aarch64
+    MAGIC='\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00'
+    MASK='\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'
+    register_binfmt $CPU $MAGIC $MASK
 
-  # registration permitted only after binaries are installed
-  echo ":qemu-${CPU}:M::${MAGIC}:${MASK}:/var/guix/profiles/per-user/offload/guix-profile/bin/qemu-${CPU}:F" > /proc/sys/fs/binfmt_misc/register
+    CPU=arm
+    MAGIC='\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00'
+    MASK='\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'
+    register_binfmt $CPU $MAGIC $MASK
+  fi
+
+  if [ "${ARCH}" != "ppc64le" ] ; then
+    CPU=ppc64le
+    MAGIC='\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x15\x00'
+    MASK='\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\x00'
+    register_binfmt $CPU $MAGIC $MASK
+  fi
+
+  if [ "${ARCH}" != "x86_64" ] ; then
+    CPU=x86_64
+    MAGIC='\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'
+    MASK='\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'
+    register_binfmt $CPU $MAGIC $MASK
+  fi
 fi
 
 
